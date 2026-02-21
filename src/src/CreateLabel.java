c2d5ee8629f81ee1996c2b922a7751bdd286bcd6
albashzia <ahmedalbashzia@gmail.com>
2026-02-21 13:23:09 +0500

    Adding picture to the label and rearranging code for better understanding.

--- src/src/CreateLabel.java
+++ src/src/CreateLabel.java
@@ -5,16 +5,17 @@ import javax.swing.ImageIcon;
 public class CreateLabel {
     public static void main() {
 
-        ImageIcon image = new ImageIcon("pc.png");
-        JFrame frame = new JFrame();
-        frame.setSize(500,500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setTitle("Labels Practice Program");
-
+        ImageIcon image = new ImageIcon("LMS.png");
 
         JLabel label = new JLabel();
         label.setText("Do you even code bro???");
+        label.setIcon(image);
+
+        JFrame frame = new JFrame();
+        frame.setSize(2500,2500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setTitle("Labels Practice Program");
 
         frame.add(label);
     }
