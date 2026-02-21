1472a9d370b878cc6d5d58bcf84d97d1532c41de
albashzia <ahmedalbashzia@gmail.com>
2026-02-21 13:11:10 +0500

    Creating CreateLabel.java and adding a basic frame to implement code to practice labels on it.

---
+++ src/src/CreateLabel.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class CreateLabel {
+    public static void main() {
+
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setTitle("Labels Practice Program");
+
+
+    }
+}
