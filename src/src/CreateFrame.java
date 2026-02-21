d8290219a3c111502b95039928373f693f97081c
albashzia <ahmedalbashzia@gmail.com>
2026-02-21 08:53:38 +0500

    Adding a CreateFrame.java class and writing basic code to import javax.swing.JFrame and creating a frame setting a visibility and size.

---
+++ src/src/CreateFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class CreateFrame {
+    public static void main() {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(420,420);
+    }
+}
