167bfef78110b83135547c6cfcf34f39a5d1a08c
albashzia <ahmedalbashzia@gmail.com>
2026-02-21 09:28:09 +0500

    Enhancing readability of CreateFrame.java by adding comments for all the major methods used.

--- src/src/CreateFrame.java
+++ src/src/CreateFrame.java
@@ -3,17 +3,20 @@ import java.awt.Color;
 
 public class CreateFrame {
     public static void main() {
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setSize(420,420);
-        frame.setTitle("First JFrame Program");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setVisible(true); // makes frame visible
+        frame.setSize(420,420); // sets the x-dimension and y-dimension of frame
+        frame.setTitle("First JFrame Program"); // sets title of frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
+        frame.setResizable(false); // prevents from being resized
 
-        ImageIcon image = new ImageIcon("LMS.png");
-        frame.setIconImage(image.getImage());
-        //frame.getContentPane().setBackground(Color.GREEN);
-        //frame.getContentPane().setBackground(new Color(123,50,250));
-        frame.getContentPane().setBackground(new Color(0x000000));
+        ImageIcon image = new ImageIcon("LMS.png"); // creates an ImageIcon
+        frame.setIconImage(image.getImage()); // change icon of frame
+
+        // DIFFERENT WAYS OF SETTING BACKGROUND COLOR
+
+        //frame.getContentPane().setBackground(Color.GREEN); // setting background color using color import
+        //frame.getContentPane().setBackground(new Color(123,50,250)); // setting background color using RGB values
+        frame.getContentPane().setBackground(new Color(0x000000)); // setting background color using hexadecimal values
     }
 }
