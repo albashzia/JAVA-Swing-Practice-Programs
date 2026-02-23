db011fda1456538b4cdd00651e5fa19072865abd
albashzia <ahmedalbashzia@gmail.com>
2026-02-23 10:50:17 +0500

    Creating a bluePanel and adding it to the frame.

--- src/src/CreatePanel.java
+++ src/src/CreatePanel.java
@@ -9,11 +9,18 @@ public class CreatePanel {
         redPanel.setBackground(Color.red);
         redPanel.setBounds(0,0,200,200);
 
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(200,0,200,200);
+
+
+
         JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setSize(550,550);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.add(redPanel);
+        frame.add(bluePanel);
     }
 }
