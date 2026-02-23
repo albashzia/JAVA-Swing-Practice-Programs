ec4bb24f1bf85b9b71a0d7b8bda3e3635ccfa732
albashzia <ahmedalbashzia@gmail.com>
2026-02-23 10:42:44 +0500

    Creating a file named CreatePanel.java to practice panels and adding a basic frame with basic methods such as size, layout and visibility etc.

---
+++ src/src/CreatePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class CreatePanel {
+    public static void main(String[] args)
+    {
+
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(550,550);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+    }
+}
