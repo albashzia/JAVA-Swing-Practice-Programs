2603cff0ceafd84697d950bf5cd2c16a8e71443e
albashzia <ahmedalbashzia@gmail.com>
2026-02-24 07:14:36 +0500

    Creating file CreateButton.java and creating a basic frame upon which buttons would be practiced.

---
+++ src/src/CreateButton.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class CreateButton {
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(null);
+    }
+}
