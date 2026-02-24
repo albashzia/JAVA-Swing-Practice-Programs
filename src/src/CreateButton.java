086439cd089e69ff4db03edd34ec616c05ce6137
albashzia <ahmedalbashzia@gmail.com>
2026-02-24 07:17:15 +0500

    Creating a button object, setting its dimension, importing necessary class and adding it to the file.

--- src/src/CreateButton.java
+++ src/src/CreateButton.java
@@ -1,12 +1,19 @@
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class CreateButton {
     public static void main(String[] args){
+
+        JButton button = new JButton();
+        button.setBounds(130,100,200,100);
+
 
         JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setLayout(null);
+
+        frame.add(button);
     }
 }
