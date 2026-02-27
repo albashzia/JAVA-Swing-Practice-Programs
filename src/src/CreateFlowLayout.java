9af6e1d05bb4fcf84859ebfba1f6a2ee5eb5eaea
albashzia <ahmedalbashzia@gmail.com>
2026-02-27 21:08:39 +0500

    Creating buttons and adding the to the frame.

--- src/src/CreateFlowLayout.java
+++ src/src/CreateFlowLayout.java
@@ -10,5 +10,15 @@ public class CreateFlowLayout {
         frame.setLayout(new FlowLayout());
         frame.setSize(500,500);
 
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+
     }
 }
