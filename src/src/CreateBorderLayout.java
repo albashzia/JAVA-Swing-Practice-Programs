e80acaa4b48845c2faed4f6114e5fead30b96d96
albashzia <ahmedalbashzia@gmail.com>
2026-02-27 15:44:38 +0500

    Creating a basic frame on which to practice BorderLayout concepts.

--- src/src/CreateBorderLayout.java
+++ src/src/CreateBorderLayout.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class CreateBorderLayout {
     public static void main(String args[]){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+
 
     }
 }
