344e6efb3e1f15f8cfccaaa556a7c79df1572d06
albashzia <ahmedalbashzia@gmail.com>
2026-02-27 21:04:36 +0500

    Creating a basic frame to work on during the practice of flow layout concepts.

--- src/src/CreateFlowLayout.java
+++ src/src/CreateFlowLayout.java
@@ -4,6 +4,10 @@ import javax.swing.JButton;
 
 public class CreateFlowLayout {
     public static void main(String args[]){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(500,500);
 
     }
 }
