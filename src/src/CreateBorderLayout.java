646986dc61903f335cc7bc4a2aa2a8a403369c6f
albashzia <ahmedalbashzia@gmail.com>
2026-02-27 15:46:12 +0500

    Creating 5 panel objects.

--- src/src/CreateBorderLayout.java
+++ src/src/CreateBorderLayout.java
@@ -7,6 +7,11 @@ public class CreateBorderLayout {
         frame.setSize(500,500);
         frame.setVisible(true);
 
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
+        JPanel panel5 = new JPanel();
 
     }
 }
