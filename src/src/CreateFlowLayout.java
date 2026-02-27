8e8a819aa20017a489c719a0060e63f2a4d81df8
albashzia <ahmedalbashzia@gmail.com>
2026-02-27 21:18:42 +0500

    Adding the buttons to the panel instead of frame.

--- src/src/CreateFlowLayout.java
+++ src/src/CreateFlowLayout.java
@@ -15,15 +15,15 @@ public class CreateFlowLayout {
         panel.setPreferredSize(new Dimension(250,250));
         panel.setBackground(Color.DARK_GRAY);
 
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
 
     }
 }
