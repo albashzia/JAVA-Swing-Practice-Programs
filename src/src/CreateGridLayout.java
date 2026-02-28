b8a349559234af2f835fe56210b10b46ace9b169
albashzia <ahmedalbashzia@gmail.com>
2026-02-28 05:29:33 +0500

    Creating a basic frame to practice grid layout concepts on it.

--- src/src/CreateGridLayout.java
+++ src/src/CreateGridLayout.java
@@ -5,5 +5,10 @@ import javax.swing.JButton;
 public class CreateGridLayout {
     public static void main(String args[]){
 
+        JFrame frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 }
