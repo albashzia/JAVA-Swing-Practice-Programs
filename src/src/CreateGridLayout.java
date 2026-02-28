620baf6064779e8528c3cefc3c064322665d3d1f
albashzia <ahmedalbashzia@gmail.com>
2026-02-28 05:30:52 +0500

    Creating and adding buttons to the frame.

--- src/src/CreateGridLayout.java
+++ src/src/CreateGridLayout.java
@@ -10,5 +10,15 @@ public class CreateGridLayout {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+
     }
 }
