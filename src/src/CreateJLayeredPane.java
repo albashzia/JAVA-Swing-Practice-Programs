f59a650db4a884db85eedcd1b2a957b96eabae03
albashzia <ahmedalbashzia@gmail.com>
2026-03-01 23:17:16 +0500

    Modifying the size of the labels.

--- src/src/CreateJLayeredPane.java
+++ src/src/CreateJLayeredPane.java
@@ -6,17 +6,17 @@ public class CreateJLayeredPane {
 
         JLabel label1 = new JLabel();
         label1.setOpaque(true);
-        label1.setBounds(50,50,100,100);
+        label1.setBounds(50,50,200,200);
         label1.setBackground(Color.RED);
 
         JLabel label2 = new JLabel();
         label2.setOpaque(true);
-        label2.setBounds(100,100,100,100);
+        label2.setBounds(100,100,200,200);
         label2.setBackground(Color.GREEN);
         
         JLabel label3 = new JLabel();
         label3.setOpaque(true);
-        label3.setBounds(150,150,100,100);
+        label3.setBounds(150,150,200,200);
         label3.setBackground(Color.BLUE);
 
         JLayeredPane layeredPane = new JLayeredPane();
