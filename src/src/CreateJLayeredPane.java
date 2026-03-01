3d8fce95fe20d7ce9e5e5941105bdb27b3d6181e
albashzia <ahmedalbashzia@gmail.com>
2026-03-01 22:54:40 +0500

    Creating a basic JFrame object to practice JLayeredPane on it.

--- src/src/CreateJLayeredPane.java
+++ src/src/CreateJLayeredPane.java
@@ -4,5 +4,10 @@ import javax.swing.*;
 public class CreateJLayeredPane {
     public static void main(String args[]){
 
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
