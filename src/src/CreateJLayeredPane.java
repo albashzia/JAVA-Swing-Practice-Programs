76b66f0c9a3bb434659e11890b00c22a887b87a2
albashzia <ahmedalbashzia@gmail.com>
2026-03-01 23:14:08 +0500

    Creating 3 JLabel instances with defined attributes to add to the JLayeredPane instance.

--- src/src/CreateJLayeredPane.java
+++ src/src/CreateJLayeredPane.java
@@ -4,6 +4,21 @@ import javax.swing.*;
 public class CreateJLayeredPane {
     public static void main(String args[]){
 
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBounds(50,50,100,100);
+        label1.setBackground(Color.RED);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBounds(100,100,100,100);
+        label2.setBackground(Color.GREEN);
+        
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBounds(150,150,100,100);
+        label3.setBackground(Color.BLUE);
+
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setBounds(0,0,500,500);
 
