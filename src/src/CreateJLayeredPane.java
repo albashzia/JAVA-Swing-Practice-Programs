266cc36b943fcc9cba93a06f9bcc4e8a1f28337e
albashzia <ahmedalbashzia@gmail.com>
2026-03-01 23:23:27 +0500

    Adding the position of the layers using layer numbers along with wrapper class Integer to pass the number.

--- src/src/CreateJLayeredPane.java
+++ src/src/CreateJLayeredPane.java
@@ -22,9 +22,13 @@ public class CreateJLayeredPane {
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setBounds(0,0,500,500);
 
-        layeredPane.add(label1,JLayeredPane.DEFAULT_LAYER);
-        layeredPane.add(label2,JLayeredPane.DEFAULT_LAYER);
-        layeredPane.add(label3,JLayeredPane.DRAG_LAYER);
+//        layeredPane.add(label1,JLayeredPane.DEFAULT_LAYER);
+//        layeredPane.add(label2,JLayeredPane.DEFAULT_LAYER);
+//        layeredPane.add(label3,JLayeredPane.DRAG_LAYER);
+
+        layeredPane.add(label1,Integer.valueOf(0));
+        layeredPane.add(label2,Integer.valueOf(0));
+        layeredPane.add(label3,Integer.valueOf(1));
 
         JFrame frame = new JFrame();
         frame.add(layeredPane);
