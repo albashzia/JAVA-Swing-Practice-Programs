f5f21daa920adf42ab1840ecbb12eac53c28bc28
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 15:38:36 +0500

    Commenting question message and instead displaying warning message.

--- src/src/CreateJOptionPane.java
+++ src/src/CreateJOptionPane.java
@@ -5,7 +5,8 @@ public class CreateJOptionPane {
 
         //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.PLAIN_MESSAGE );
         //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.INFORMATION_MESSAGE );
-        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.QUESTION_MESSAGE );
+        //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.QUESTION_MESSAGE );
+        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.WARNING_MESSAGE );
 
     }
 }
