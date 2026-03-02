d4d449b57dace106a95699ccae7839d4b4444c43
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 15:34:36 +0500

    Creating a basic dialog box with no parent component, some text to display, a title and Plain Message as message type.

--- src/src/CreateJOptionPane.java
+++ src/src/CreateJOptionPane.java
@@ -3,5 +3,6 @@ import javax.swing.JOptionPane;
 public class CreateJOptionPane {
     public static void main(String args[]){
 
+        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.PLAIN_MESSAGE );
     }
 }
