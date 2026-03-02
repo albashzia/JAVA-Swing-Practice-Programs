6d7d09d53eab887832def3ca3237f9e9ee8f9066
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 15:37:18 +0500

    Commenting plain message and instead displaying information message.

--- src/src/CreateJOptionPane.java
+++ src/src/CreateJOptionPane.java
@@ -3,6 +3,8 @@ import javax.swing.JOptionPane;
 public class CreateJOptionPane {
     public static void main(String args[]){
 
-        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.PLAIN_MESSAGE );
+        //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.INFORMATION_MESSAGE );
+
     }
 }
