545c13b0fd6bfa1e2c2517977cf99b7fe3b75280
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 07:34:46 +0500

    Defining attributes for the button, implementing ActionListener and creating the unimplemented method for ActionListener

--- src/CreateLaunchPage/src/LaunchPage.java
+++ src/CreateLaunchPage/src/LaunchPage.java
@@ -1,16 +1,28 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LaunchPage {
+public class LaunchPage implements ActionListener  {
 
     JFrame frame = new JFrame();
     JButton button = new JButton();
 
     LaunchPage(){
 
+        button.setBounds(100,160,200,40);
+        button.setFocusable(false);
+        button.addActionListener(this);
+
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setSize(420,420);
         frame.setLayout(null);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
 
     }
 }
