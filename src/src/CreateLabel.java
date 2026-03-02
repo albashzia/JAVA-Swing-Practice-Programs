2ccde9751b566e09880832af0cd7460584d97212
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 07:03:10 +0500

    Updating documentation of  CreateLabel.java

--- src/src/CreateLabel.java
+++ src/src/CreateLabel.java
@@ -10,13 +10,16 @@ public class CreateLabel {
         Border border = BorderFactory.createLineBorder(Color.BLACK,3); // creating a border
 
         // LABEL
+
         JLabel label = new JLabel(); // creating an instance of JLabel
         label.setText("Do you even code bro???"); // setting text on the label
         label.setIcon(image); // setting image for the label using ImageIcon object
         label.setHorizontalTextPosition(JLabel.CENTER); // setting horizontal position of the text
         label.setVerticalTextPosition(JLabel.TOP); // setting vertical position of the text
 
-        // Different ways to set background color
+
+        // SETTING BACKGROUND COLOR USING DIFFERENT METHODS
+
         //label.setForeground(Color.BLUE); // using Color class
         //label.setForeground(new Color(20,200,60)); // using RGB values
         label.setForeground(new Color(0x000333)); // using hexadecimal coding
@@ -26,6 +29,7 @@ public class CreateLabel {
         label.setIconTextGap(70); // setting the gap between the text and icon on the label
 
         label.setBackground(Color.cyan); // setting background color of the label
+
         label.setOpaque(true); // setting opaqueness of the label
 
         label.setBorder(border); // adding the created border instance to the label
@@ -36,6 +40,7 @@ public class CreateLabel {
         //label.setBounds(0,0,450,450);
 
         // FRAME
+
         JFrame frame = new JFrame(); // creating the frame instance
         //frame.setSize(500,500);
         frame.setResizable(false); // restricting the resizability of the frame
@@ -43,7 +48,9 @@ public class CreateLabel {
         frame.setVisible(true); // setting visibility
         frame.setTitle("Labels Practice Program"); // setting title of the frame
         //frame.setLayout(null);
+
         frame.add(label); // adding the label to the frame
+
         frame.pack();
     }
 }
