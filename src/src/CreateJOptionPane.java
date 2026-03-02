8c805f63c6d374dfc51d42324915413ccc76a33a
albashzia <ahmedalbashzia@gmail.com>
2026-03-02 15:37:59 +0500

    Commenting information message and instead displaying question message.

--- src/src/CreateJOptionPane.java
+++ src/src/CreateJOptionPane.java
@@ -4,7 +4,8 @@ public class CreateJOptionPane {
     public static void main(String args[]){
 
         //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.PLAIN_MESSAGE );
-        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.INFORMATION_MESSAGE );
+        //JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.INFORMATION_MESSAGE );
+        JOptionPane.showMessageDialog(null,"This is a message","Title", JOptionPane.QUESTION_MESSAGE );
 
     }
 }
