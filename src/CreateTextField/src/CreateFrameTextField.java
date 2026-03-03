604cf538f6751b387303904b6ad5b8f08ba2bfc3
albashzia <ahmedalbashzia@gmail.com>
2026-03-03 08:54:47 +0500

    Defining basic properties of the frame in a constructor using this keyword.

--- src/CreateTextField/src/CreateFrameTextField.java
+++ src/CreateTextField/src/CreateFrameTextField.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 
+import java.awt.*;
 import java.awt.event.*;
 
 public class CreateFrameTextField extends JFrame implements ActionListener{
 
     CreateFrameTextField(){
 
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+        this.pack();
 
     }
 
