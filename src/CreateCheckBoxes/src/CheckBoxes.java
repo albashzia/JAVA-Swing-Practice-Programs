173c014fc04b727c3413a15dcf74a7f57f4bdc17
albashzia <ahmedalbashzia@gmail.com>
2026-03-03 21:02:44 +0500

    Making necessary imports, extending from JFrame, implementing ActionListener and Overriding actionPerformed method

--- src/CreateCheckBoxes/src/CheckBoxes.java
+++ src/CreateCheckBoxes/src/CheckBoxes.java
@@ -1,2 +1,14 @@
-public class CheckBoxes {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CheckBoxes extends JFrame implements ActionListener {
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
 }
