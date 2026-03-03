2d41eeb00e7c8b37c678d1a2f291c2d33f0ad8c1
albashzia <ahmedalbashzia@gmail.com>
2026-03-03 21:04:54 +0500

    Creating a constructor which contains the basic methods to initialize the frame.

---
+++ src/CreateCheckBoxes/src/CheckBoxesFrame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CheckBoxesFrame extends JFrame implements ActionListener {
+
+    CheckBoxesFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+        this.pack();
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
