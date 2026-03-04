bac8e0315e53bcdc11b0edc20a3c457967310317
albashzia <ahmedalbashzia@gmail.com>
2026-03-04 05:32:36 +0500

    Adding necessary imports, extending JFrame, implementing ActionListener and Overriding actionPerformed method in RadioButtonFrame class.

--- src/CreateRadioButton/src/RadioButtonFrame.java
+++ src/CreateRadioButton/src/RadioButtonFrame.java
@@ -1,2 +1,15 @@
-public class RadioButtonFrame {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class RadioButtonFrame extends JFrame implements ActionListener{
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
 }
