2bb20553c1b4526f50708c8fff484f6d2bc883ca
albashzia <ahmedalbashzia@gmail.com>
2026-03-04 06:48:49 +0500

    Add default close operation, FlowLayout manager, visibility, and dynamic sizing using pack()

--- src/CreateRadioButton/src/RadioButtonFrame.java
+++ src/CreateRadioButton/src/RadioButtonFrame.java
@@ -4,9 +4,12 @@ import java.awt.*;
 
 public class RadioButtonFrame extends JFrame implements ActionListener{
 
-
-
-
+    RadioButtonFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLayout(new FlowLayout());
+        this.pack();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e){
