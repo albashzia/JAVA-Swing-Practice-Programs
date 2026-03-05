e035c96714e4d4c3b3626acd6cb01feb054cdfd4
albashzia <ahmedalbashzia@gmail.com>
2026-03-05 22:39:26 +0500

    Creating JRadioButtons.

--- src/CreateRadioButton/src/RadioButtonFrame.java
+++ src/CreateRadioButton/src/RadioButtonFrame.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import javax.swing.JRadioButton;
 
 public class RadioButtonFrame extends JFrame implements ActionListener{
 
     RadioButtonFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        JRadioButton pizzaButton = new JRadioButton("Pizza");
+        JRadioButton burgerButton = new JRadioButton("Burger");
+        JRadioButton hotdogButton = new JRadioButton("HotDog");
+
         this.setLayout(new FlowLayout());
         this.pack();
     }
