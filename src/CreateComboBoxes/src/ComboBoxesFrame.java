7c75aab30bd1086384fe48e64be37866f6164e33
albashzia <ahmedalbashzia@gmail.com>
2026-03-06 19:51:02 +0500

    Extending JFrame, implementing ActionListener, Overriding actionPerformed method and making necessary imports in ComboBoxesFrame class

--- src/CreateComboBoxes/src/ComboBoxesFrame.java
+++ src/CreateComboBoxes/src/ComboBoxesFrame.java
@@ -1,2 +1,14 @@
-public class ComboBoxesFrame {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComboBoxesFrame extends JFrame implements ActionListener {
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
 }
