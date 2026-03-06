08e8e8aa9ca9f181bd87c391dcfbd04e56584a6c
albashzia <ahmedalbashzia@gmail.com>
2026-03-06 19:52:32 +0500

    Defining the ComboBoxesFrame constructor with the methods to create a basic frame on creating the instance of ComboBoxesFrame.

--- src/CreateComboBoxes/src/ComboBoxesFrame.java
+++ src/CreateComboBoxes/src/ComboBoxesFrame.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ComboBoxesFrame extends JFrame implements ActionListener {
 
+    ComboBoxesFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
 
+        this.pack();
+        this.setVisible(true);
+    }
 
 
     @Override
