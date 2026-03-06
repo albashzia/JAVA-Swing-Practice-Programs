3d8ac9036eda738ec74a66fdcb8471acb6a35baa
albashzia <ahmedalbashzia@gmail.com>
2026-03-06 23:50:26 +0500

    Setting the editability of the comboBox to be true

--- src/CreateComboBoxes/src/ComboBoxesFrame.java
+++ src/CreateComboBoxes/src/ComboBoxesFrame.java
@@ -13,7 +13,7 @@ public class ComboBoxesFrame extends JFrame implements ActionListener {
 
         String[] animals = {"dog","cat","bird"};
         comboBox = new JComboBox(animals);
-
+        comboBox.setEditable(true);
         comboBox.addActionListener(this);
         this.add(comboBox);
         this.pack();
