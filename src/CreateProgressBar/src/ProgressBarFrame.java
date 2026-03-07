39d746755337157802bcba7c3f64591005aeb2fd
albashzia <ahmedalbashzia@gmail.com>
2026-03-07 16:48:29 +0500

    Instantiating a JFrame and JProgressBar object along with a ProgressBarFrame constructor definition.

--- src/CreateProgressBar/src/ProgressBarFrame.java
+++ src/CreateProgressBar/src/ProgressBarFrame.java
@@ -2,5 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ProgressBarFrame {
+    JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
 
+    ProgressBarFrame(){
+
+    }
 }
