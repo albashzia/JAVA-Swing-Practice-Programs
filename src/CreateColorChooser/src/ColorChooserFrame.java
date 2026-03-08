f502b0c1748f9bff199a445a33ca00cd4d78c29a
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 16:57:30 +0500

    Creating a ColorChooserFrame constructor with basic attributes to initialize a frame.

--- src/CreateColorChooser/src/ColorChooserFrame.java
+++ src/CreateColorChooser/src/ColorChooserFrame.java
@@ -4,6 +4,13 @@ import java.awt.event.*;
 
 public class ColorChooserFrame extends JFrame implements ActionListener {
 
+    ColorChooserFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        this.pack();
+        this.setVisible(true);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e){
