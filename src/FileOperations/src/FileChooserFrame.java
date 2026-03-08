3e94a2d9abc2769e36b2aba7c0d6faffda07c7df
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 12:41:40 +0500

    Storing the response of showOpenDialog method in an int variable and displaying it.

--- src/FileOperations/src/FileChooserFrame.java
+++ src/FileOperations/src/FileChooserFrame.java
@@ -22,7 +22,8 @@ public class FileChooserFrame extends JFrame implements ActionListener {
         if (e.getSource()==button){
             JFileChooser fileChooser = new JFileChooser();
 
-            fileChooser.showOpenDialog(null);
+            int response = fileChooser.showOpenDialog(null);
+            System.out.println(response);
         }
     }
 }
