85e286dc61be4575df98bf511aab5894a1f1c52a
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 16:59:33 +0500

    Instantiating a JButton and a JLabel.

--- src/CreateColorChooser/src/ColorChooserFrame.java
+++ src/CreateColorChooser/src/ColorChooserFrame.java
@@ -4,9 +4,15 @@ import java.awt.event.*;
 
 public class ColorChooserFrame extends JFrame implements ActionListener {
 
+    JButton button;
+    JLabel label;
+
     ColorChooserFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+
+        button = new JButton("Pick a color");
+        label = new JLabel();
 
         this.pack();
         this.setVisible(true);
