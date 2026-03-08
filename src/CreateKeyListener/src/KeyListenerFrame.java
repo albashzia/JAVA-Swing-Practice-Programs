1cf61d140a59bdf37acbb3c8d5b5c4cb1a147def
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 20:57:20 +0500

    Defining a KeyListenerFrame constructor with basic methods to initialize a frame.

--- src/CreateKeyListener/src/KeyListenerFrame.java
+++ src/CreateKeyListener/src/KeyListenerFrame.java
@@ -3,4 +3,11 @@ import java.awt.Color;
 import java.awt.event.*;
 
 public class KeyListenerFrame extends JFrame {
+
+    KeyListenerFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
 }
