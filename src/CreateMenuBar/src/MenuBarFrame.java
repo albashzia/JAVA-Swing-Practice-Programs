1e7377471dfc47b2bc1421e12eb8c90dd95c416e
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 11:19:01 +0500

    Defining a MenuBarFrame constructor with basic properties of a JFrame

--- src/CreateMenuBar/src/MenuBarFrame.java
+++ src/CreateMenuBar/src/MenuBarFrame.java
@@ -4,16 +4,13 @@ import javax.swing.*;
 
 public class MenuBarFrame extends JFrame implements ActionListener{
 
+    MenuBarFrame(){
 
-
-
-
-
-
-
-
-
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+        this.setVisible(true);
+    }
 
 
 
