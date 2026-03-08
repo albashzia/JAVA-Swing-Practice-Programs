be33fbce408a672720e5b7f8b634e7a73390c21d
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 12:32:20 +0500

    Defining FileChooserFrame constructor with basic attributes to create a frame.

--- src/FileOperations/src/FileChooserFrame.java
+++ src/FileOperations/src/FileChooserFrame.java
@@ -1,9 +1,18 @@
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class FileChooserFrame extends JFrame implements ActionListener {
 
+    FileChooserFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
 
+
+
+        this.pack();
+        this.setVisible(true);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e){
