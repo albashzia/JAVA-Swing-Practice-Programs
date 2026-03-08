a2fb1a95f82ac77f33906885ff7909b4ed34f41f
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 20:59:10 +0500

    Implementing KeyListener and overriding keyTyped, keyPressed and keyReleased methods.

--- src/CreateKeyListener/src/KeyListenerFrame.java
+++ src/CreateKeyListener/src/KeyListenerFrame.java
@@ -2,12 +2,27 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
 
-public class KeyListenerFrame extends JFrame {
+public class KeyListenerFrame extends JFrame implements KeyListener {
 
     KeyListenerFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
         this.setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+
     }
 }
