e11362d1f146792145e84671d8cefcc63e5e732d
albashzia <ahmedalbashzia@gmail.com>
2026-03-08 12:33:39 +0500

    Creating an instance of JButton outside constructor, instantiating it inside the constructor and adding it to the frame.

--- src/FileOperations/src/FileChooserFrame.java
+++ src/FileOperations/src/FileChooserFrame.java
@@ -4,11 +4,14 @@ import javax.swing.*;
 
 public class FileChooserFrame extends JFrame implements ActionListener {
 
+    JButton button;
     FileChooserFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
+        button = new JButton("Select File");
 
+        this.add(button);
 
         this.pack();
         this.setVisible(true);
