8a0328cc2b2027950f79a30830710e19734b03bd
albashzia <ahmedalbashzia@gmail.com>
2026-03-09 16:01:42 +0500

    Refactoring the code in DragAndDropFrame class, removing a bug

--- src/DragAndDropImages/src/DragAndDropFrame.java
+++ src/DragAndDropImages/src/DragAndDropFrame.java
@@ -2,14 +2,14 @@ import javax.swing.*;
 
 public class DragAndDropFrame extends JFrame{
 
-    DragAndDropPanel dragPanel = new DragAndDropPanel();
+    DragAndDropPanel dragPanel;
 
     DragAndDropFrame(){
 
+        dragPanel  = new DragAndDropPanel();
         this.add(dragPanel);
 
         this.setTitle("Drag and Drop Demo");
-        this.setLayout(null);
         this.setSize(600,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
