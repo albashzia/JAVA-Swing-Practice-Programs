9845295c365665fd417bed95e4800c51a318329a
albashzia <ahmedalbashzia@gmail.com>
2026-03-09 06:40:00 +0500

    Implementing MouseListener and overriding mouseClicked, mousePressed, mouseReleased, mouseEntered and mouseExited methods

--- src/CreateMouseListener/src/MouseListenerFrame.java
+++ src/CreateMouseListener/src/MouseListenerFrame.java
@@ -2,13 +2,33 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MouseListenerFrame extends JFrame {
+public class MouseListenerFrame extends JFrame implements MouseListener{
 
     MouseListenerFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
+    }
+    @Override
+    public void mouseClicked(MouseEvent e){
+
+    }
+    @Override
+    public void mousePressed(MouseEvent e){
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent e){
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e){
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e){
+
     }
 
 }
