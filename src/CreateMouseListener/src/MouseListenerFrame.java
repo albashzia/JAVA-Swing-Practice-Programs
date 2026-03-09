8a044b417aeddedb3d0c0e77d5302dba4446b5e5
albashzia <ahmedalbashzia@gmail.com>
2026-03-09 06:36:58 +0500

    Creating a MouseListenerFrame constructor with basic methods to create a frame

--- src/CreateMouseListener/src/MouseListenerFrame.java
+++ src/CreateMouseListener/src/MouseListenerFrame.java
@@ -4,4 +4,11 @@ import javax.swing.*;
 
 public class MouseListenerFrame extends JFrame {
 
+    MouseListenerFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+
 }
