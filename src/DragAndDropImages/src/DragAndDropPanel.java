062f020a1a015c10229e2c7af5aba8efd58053b7
albashzia <ahmedalbashzia@gmail.com>
2026-03-09 11:12:16 +0500

    Creating an inner class named ClickListener which extend MouseAdapter class defined in java.awt package

--- src/DragAndDropImages/src/DragAndDropPanel.java
+++ src/DragAndDropImages/src/DragAndDropPanel.java
@@ -1,10 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
 
 public class DragAndDropPanel extends JPanel {
 
     ImageIcon icon = new ImageIcon("smile_120px.png");
 
     DragAndDropPanel(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    private class ClickListener extends MouseAdapter{
 
     }
 }
