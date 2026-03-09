bc6240e0b428439b1a54a8c03a04d725c48901ac
albashzia <ahmedalbashzia@gmail.com>
2026-03-09 10:58:50 +0500

    Creating a DragAndDropFrame with basic methods to create a frame.

--- src/DragAndDropImages/src/DragAndDropFrame.java
+++ src/DragAndDropImages/src/DragAndDropFrame.java
@@ -1,4 +1,11 @@
 import javax.swing.*;
 
 public class DragAndDropFrame extends JFrame{
+    DragAndDropFrame(){
+        this.setTitle("Drag and Drop Demo");
+        this.setLayout(null);
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 }
