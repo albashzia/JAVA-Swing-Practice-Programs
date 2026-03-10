df10b0835b1b4f6eaaf591bd2650078725454603
albashzia <ahmedalbashzia@gmail.com>
2026-03-10 19:38:55 +0500

    Creating an instantiating GraphicsPanel object in GraphicsFrame and adding it to the frame with necessary adjustments

--- 2DGraphics/src/GraphicsFrame.java
+++ 2DGraphics/src/GraphicsFrame.java
@@ -3,9 +3,15 @@ import javax.swing.*;
 
 public class GraphicsFrame extends JFrame{
 
+    GraphicsPanel panel;
+
     GraphicsFrame(){
+
+        panel = new GraphicsPanel();
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
+        this.add(panel);
+        this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
