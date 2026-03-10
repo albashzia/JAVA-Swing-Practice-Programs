edfcaf2357420f604db382cf4ac6a049d02339d9
albashzia <ahmedalbashzia@gmail.com>
2026-03-10 19:28:26 +0500

    Instantiating a basic frame inside GraphicsFrame constructor.

--- 2DGraphics/src/GraphicsFrame.java
+++ 2DGraphics/src/GraphicsFrame.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class GraphicsFrame extends JFrame{
 
     GraphicsFrame(){
-
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
