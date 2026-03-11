0506f32a2be64fe99192f6d0d41c63f3915cbca3
albashzia <ahmedalbashzia@gmail.com>
2026-03-11 16:40:33 +0500

    Using fillOval method to draw a filled circle passing coordinates and dimensions as parameters

--- 2DGraphics/src/GraphicsPanel.java
+++ 2DGraphics/src/GraphicsPanel.java
@@ -15,7 +15,8 @@ public class GraphicsPanel extends JPanel {
         //g2d.drawLine(0,0,500,500);
         g2d.drawRect(0,0,100,100);
         g2d.fillRect(150,0,100,100);
-        
+
         g2d.drawOval(10,120,100,100);
+        g2d.fillOval(130,120,100,100);
     }
 }
