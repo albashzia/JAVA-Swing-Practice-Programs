6cad99a8faba0087e22cdb8efac1daa26842b09c
albashzia <ahmedalbashzia@gmail.com>
2026-03-11 16:46:46 +0500

    Drawing a half circle using drawArc method

--- 2DGraphics/src/GraphicsPanel.java
+++ 2DGraphics/src/GraphicsPanel.java
@@ -13,11 +13,13 @@ public class GraphicsPanel extends JPanel {
         g2d.setPaint(Color.BLUE);
         g2d.setStroke(new BasicStroke(5));
         //g2d.drawLine(0,0,500,500);
-        g2d.drawRect(0,0,100,100);
-        g2d.fillRect(150,0,100,100);
+        //g2d.drawRect(0,0,100,100);
+        //g2d.fillRect(150,0,100,100);
 
-        g2d.setPaint(Color.yellow);
-        g2d.drawOval(10,120,100,100);
-        g2d.fillOval(130,120,100,100);
+        //g2d.setPaint(Color.yellow);
+        //g2d.drawOval(10,120,100,100);
+        //g2d.fillOval(130,120,100,100);
+
+        g2d.drawArc(0,0,100,100,0,180);
     }
 }
