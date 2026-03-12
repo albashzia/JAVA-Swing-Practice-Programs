c94ee703e5019219137639ccfe054bebbd4d46e8
albashzia <ahmedalbashzia@gmail.com>
2026-03-12 06:21:53 +0500

    Making necessary imports, extending JFrame, defining a constructor and adding methods to create a basic frame in AnimationsFrame class

--- src/2DAnimations/src/AnimationsFrame.java
+++ src/2DAnimations/src/AnimationsFrame.java
@@ -1,2 +1,13 @@
-public class AnimationsFrame {
+import java.awt.*;
+import javax.swing.*;
+
+public class AnimationsFrame extends JFrame{
+
+    AnimationsFrame(){
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 }
