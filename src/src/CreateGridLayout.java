955ea2f8a6d55fdb9b1035827251a22cd2747521
albashzia <ahmedalbashzia@gmail.com>
2026-03-14 17:48:58 +0500

    Documenting the CreateGridLayout.java file with added comments.

--- src/src/CreateGridLayout.java
+++ src/src/CreateGridLayout.java
@@ -4,13 +4,11 @@ import javax.swing.JButton;
 
 public class CreateGridLayout {
     public static void main(String args[]){
-
-        JFrame frame = new JFrame();
-        frame.setSize(500,500);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(3,3,10,10));
-        frame.setVisible(true);
-
+        JFrame frame = new JFrame();//instantiating the frame
+        frame.setSize(500,500);//setting the size of the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//setting close operation
+        frame.setLayout(new GridLayout(3,3,10,10));//arranges components in a grid with 3 rows,3 columns and 10px spacing between components
+        //adding 9 button components to the frame
         frame.add(new JButton("1"));
         frame.add(new JButton("2"));
         frame.add(new JButton("3"));
@@ -20,6 +18,6 @@ public class CreateGridLayout {
         frame.add(new JButton("7"));
         frame.add(new JButton("8"));
         frame.add(new JButton("9"));
-
+        frame.setVisible(true);//setting the visibility of the frame
     }
 }
