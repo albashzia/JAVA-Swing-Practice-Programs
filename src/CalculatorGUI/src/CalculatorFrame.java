ef65e60820a46129587977e69d9ecbfbb532ad85
albashzia <ahmedalbashzia@gmail.com>
2026-04-22 15:33:32 +0500

    Adding panel, buttons and extending JFrame from the class

--- src/CalculatorGUI/src/CalculatorFrame.java
+++ src/CalculatorGUI/src/CalculatorFrame.java
@@ -1,2 +1,26 @@
-public class CalculatorFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorFrame extends JFrame {
+    public CalculatorFrame(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(4,4,5,5));
+        panel.add(new JButton("9"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("/"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("*"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("1"));
+        panel.add(new JButton("-"));
+        panel.add(new JButton("0"));
+        panel.add(new JButton("."));
+        panel.add(new JButton("="));
+        panel.add(new JButton("+"));
+        this.add(panel);
+    }
 }
