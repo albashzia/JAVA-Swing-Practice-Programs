67a8c23f5b9fbda831d42cb1acb3f7940780e8e2
albashzia <ahmedalbashzia@gmail.com>
2026-04-26 23:32:15 +0500

    Creating a Scanner method in main method of MainRunner class

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class MainRunner {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter the respective number to call the constructor:\n" +
                 "1." +
                 "\n2." +
@@ -12,5 +13,6 @@ public class MainRunner {
                 "\n8." +
                 "\n9." +
                 "\n10.");
+
     }
 }
