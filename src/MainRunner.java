2c486ac2d7beee06f73b77c07e35b19be905e2df
albashzia <ahmedalbashzia@gmail.com>
2026-04-26 23:31:12 +0500

    Adding a print statement to MainRunner class

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class MainRunner {
     public static void main(String[] args){
-
+        System.out.println("Enter the respective number to call the constructor:\n" +
+                "1." +
+                "\n2." +
+                "\n3." +
+                "\n4." +
+                "\n5." +
+                "\n6." +
+                "\n7." +
+                "\n8." +
+                "\n9." +
+                "\n10.");
     }
 }
